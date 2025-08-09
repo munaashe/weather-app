c84515fee37e4a0ab56f1eb1c0595dd1aa67e34b
Munaashe <denis.siduna@gmail.com>
2025-08-09 14:47:47 +0200

    project initialised

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-slate-700 p-4 rounded-lg">
+        here we go
+      </div>
+    </>
+  )
+}
+
+export default App
